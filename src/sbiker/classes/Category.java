112da6d63535fc291b09585d74269bc6ac67209b
hunging <zbill13091993@gmail.com>
2019-11-18 22:51:18 +0100

    add category utils

--- src/sbiker/classes/Category.java
+++ src/sbiker/classes/Category.java
@@ -5,6 +5,8 @@
  */
 package sbiker.classes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
@@ -13,10 +15,24 @@ public class Category {
 
   private final int id;
   private final String term_id;
+  private String name;
+  private String slug;
+  private final ArrayList<String> products;
 
-  public Category(int id, String term_id) {
+  public Category(int id, String term_id, String name, String slug, ArrayList<String> products) {
     this.id = id;
     this.term_id = term_id;
+    this.name = name;
+    this.slug = slug;
+    this.products = products;
+  }
+
+  public ArrayList<String> getProducts() {
+    return products;
+  }
+
+  public void addProducts(String product) {
+    this.products.add(product);
   }
 
   public int getId() {
@@ -25,6 +41,22 @@ public class Category {
 
   public String getTerm_id() {
     return term_id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSlug() {
+    return slug;
+  }
+
+  public void setSlug(String slug) {
+    this.slug = slug;
   }
 
 }
---
+++ src/utils/CategoryUtil.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sbiker.classes.Category;
+
+/**
+ *
+ * @author Admin
+ */
+public class CategoryUtil {
+
+  public static void processCategory() {
+    try {
+      ResultSet rs = null;
+      int index = 1;
+      while (rs.next()) {
+
+        AllCategories.addToCategoryList(new Category(index, rs.getString(1), rs.getString(2), rs.getString(3), new ArrayList<>()));
+        index++;
+      }
+//      AllCategories.getCategoryList().forEach((Category temp) -> {
+//
+//        System.out.println(temp.getId() + "           " + temp.getName() + "         " + temp.getTerm_id());
+//      });
+
+      AllCategories.categoryDictionaryInit();
+      ConnectionMySql.closeConnection();
+
+    } catch (SQLException ex) {
+      Logger.getLogger(CategoryUtil.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+
+}
+
+class AllCategories {
+
+  private static final ArrayList<Category> categoryList = new ArrayList<Category>();
+  private static final Map<Integer, Integer> categoryDictionary = new HashMap();
+
+  public static ArrayList<Category> getCategoryList() {
+    return categoryList;
+  }
+
+  public static int getCategoryIdFromTermId(int term_id) {
+    return categoryDictionary.get(term_id);
+  }
+
+  public static Map getCategoryDictionary() {
+    return categoryDictionary;
+  }
+
+  public static void addToCategoryList(Category e) {
+    categoryList.add(e);
+  }
+
+  public static void categoryDictionaryInit() {
+
+    getCategoryList().forEach((category) -> {
+      categoryDictionary.put(Integer.parseInt(category.getTerm_id().trim()), category.getId());
+    });
+  }
+
+}
